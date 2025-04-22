c4131155240406eeb39b86c6f0b873aa28280f1a
shinamon <76248816+shinamon610@users.noreply.github.com>
2025-04-22 10:34:31 +0900

    make utils

---
+++ FileTracer/Utils.lean
@@ -0,0 +1,26 @@
+import FileTracer.ninja
+import Lean4MyLib.DAG
+import FileTracer.BuildSystem
+open DAG
+open BuildSystem
+
+class HasFilePathAndComment (A:Type) where
+  path:A->String
+  comment:A->String
+
+def empty:StateM S Unit := return ()
+def root [Inhabited A]:  (DAG A) := DAG.Node default []
+
+def dagToTasks [HasFilePathAndComment A] [Inhabited A] (sd:StateM (DAG A) Unit):Tasks Applicative String (IO String) := fun key =>
+  let ds := (execState sd root).run
+  let condition:A ->Bool := (fun d => (HasFilePathAndComment.path d) == key)
+  match find condition ds with
+    | none => none
+    | some node =>
+      match node with
+        | DAG.Node _ [] => none
+        | DAG.Node a children =>
+          some $ BuildSystem.Task.mk fun _ fetch dirty_keys =>
+            let ks := (children <&> top) <&> (HasFilePathAndComment.path ·)
+            let fetched := (List.sequence (ks.map fetch)) <&> IO.sequence
+            (human key ks dirty_keys (HasFilePathAndComment.comment a) ) <$> fetched
--- Main.lean
+++ Main.lean
@@ -1,12 +1,11 @@
 import FileTracer.ninja
 import FileTracer.BuildSystem
 import Lean4MyLib.DAG
+import FileTracer.Utils
 open DAG
 open BuildSystem
 
-abbrev BTs := BuildSystem.Tasks
-
-def tasks:BTs Applicative String (IO String) := fun key =>
+def tasks:Tasks Applicative String (IO String) := fun key =>
   match key with
     | "./Test/output.o"=> some (BuildSystem.Task.mk fun _ fetch dirty_keys =>
       let ks: List String := ["./Test/input1.c", "./Test/input2.c"]
@@ -18,27 +17,6 @@ def tasks:BTs Applicative String (IO String) := fun key =>
       let fetched := (List.sequence (ks.map fetch)) <&> IO.sequence
       (human key ks dirty_keys "") <$> fetched)
     | _ => none
-
-class HasFilePathAndComment (A:Type) where
-  path:A->String
-  comment:A->String
-
-def empty:StateM S Unit := return ()
-def root [Inhabited A]:  (DAG A) := DAG.Node default []
-
-def dagToTasks [HasFilePathAndComment A] [Inhabited A] (sd:StateM (DAG A) Unit):BTs Applicative String (IO String) := fun key =>
-  let ds := (execState sd root).run
-  let condition:A ->Bool := (fun d => (HasFilePathAndComment.path d) == key)
-  match find condition ds with
-    | none => none
-    | some node =>
-      match node with
-        | DAG.Node _ [] => none
-        | DAG.Node a children =>
-          some $ BuildSystem.Task.mk fun _ fetch dirty_keys =>
-            let ks := (children <&> top) <&> (HasFilePathAndComment.path ·)
-            let fetched := (List.sequence (ks.map fetch)) <&> IO.sequence
-            (human key ks dirty_keys (HasFilePathAndComment.comment a) ) <$> fetched
 
 structure TestA where
   path:String
