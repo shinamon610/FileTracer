42a26b8e0cf85df2cd7844a8caae4b8cf636e74c
shinamon <76248816+shinamon610@users.noreply.github.com>
2025-09-22 13:50:12 +0900

    update

--- FileTracer/Utils.lean
+++ FileTracer/Utils.lean
@@ -1,26 +1,26 @@
 import FileTracer.ninja
-import Lean4MyLib.DAG
+import Lean4MyLib.Tree
 import FileTracer.BuildSystem
 import Lean4MyLib.MyState
 open MyState
-open DAG
+open Tree
 open BuildSystem
 
 class HasFilePathAndComment (A:Type) where
   path:A->String
   comment:A->String
 
-def root [Inhabited A]:  (DAG A) := DAG.Node default []
+def root [Inhabited A]:  (Tree A) := Tree.Node default []
 
-def dagToTasks [HasFilePathAndComment A] [Inhabited A] (sd:StateM (DAG A) Unit) (readBinIO:String ->IO ByteArray):Tasks Applicative String (IO ByteArray) := fun key =>
+def dagToTasks [HasFilePathAndComment A] [Inhabited A] (sd:StateM (Tree A) Unit) (readBinIO:String ->IO ByteArray):Tasks Applicative String (IO ByteArray) := fun key =>
   let ds := (exec sd root).run
   let condition:A ->Bool := (fun d => (HasFilePathAndComment.path d) == key)
   match find condition ds with
     | none => none
     | some node =>
       match node with
-        | DAG.Node _ [] => none
-        | DAG.Node a children =>
+        | Tree.Node _ [] => none
+        | Tree.Node a children =>
           some $ BuildSystem.Task.mk fun _ fetch dirty_keys =>
             let ks := (children <&> top) <&> (HasFilePathAndComment.path ·)
             let fetched := (List.sequence (ks.map fetch)) <&> IO.sequence
